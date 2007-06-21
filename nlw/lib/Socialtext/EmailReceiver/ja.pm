e4c9ea8f78c571f3741bc05d39bf58a4ad9fe42b
kitano-san <dev@socialtext.com>
2007-06-21 19:02:09 +0000

    remove DT::F::J dependency.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/i18n-p3@3310 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/EmailReceiver/ja.pm
+++ nlw/lib/Socialtext/EmailReceiver/ja.pm
@@ -7,23 +7,18 @@ use warnings;
 use base 'Socialtext::EmailReceiver::Base';
 use Socialtext::l10n qw(loc system_locale);
 
-use DateTime::Format::Japanese
-    qw(FORMAT_KANJI FORMAT_ERA FORMAT_GREGORIAN FORMAT_ROMAN);
+use DateTime::Format::Strptime;
+use utf8;
 
 sub format_date {
     my $self     = shift;
     my $datetime = shift;
     my $date_header;
 
-    my $fmt = DateTime::Format::Japanese->new(
-        number_format         => FORMAT_ROMAN,
-        year_format           => FORMAT_ROMAN,
-        with_gregorian_marker => 0,
-        with_bc_marker        => 0,
-        with_ampm_marker      => 0,
-        with_day_of_week      => 1,
-        input_encoding        => 'utf8',
-        output_encoding       => 'utf8'
+
+    my $fmt = DateTime::Format::Strptime->new(
+        pattern   => '%Y年%m月%d日%H時%M分%S秒',
+        locale    => 'ja',
     );
 
     $date_header = $fmt->format_datetime($datetime);
