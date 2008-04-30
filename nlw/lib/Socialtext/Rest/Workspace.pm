f0d83effd34d812bd6c222b6d99278095f0cfc4a
Luke Closs <gravatar@5thplane.com>
2008-04-30 01:09:39 +0000

    Allow setting customjs_uri via REST
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/iteration-2008-05-02@7253 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/Rest/Workspace.pm
+++ nlw/lib/Socialtext/Rest/Workspace.pm
@@ -5,6 +5,8 @@ use strict;
 use warnings;
 
 use base 'Socialtext::Rest::Entity';
+use Socialtext::HTTP ':codes';
+use JSON::XS;
 
 sub permission      { +{ GET => 'read' } }
 sub allowed_methods {'GET, HEAD'}
@@ -60,6 +62,26 @@ sub get_resource {
                       : { &$extra_data, &$peon_view };
 }
 
+sub PUT {
+    my( $self, $rest ) = @_;
+
+    unless ($self->_user_is_business_admin_p( ) ) {
+        $rest->header(
+                      -status => HTTP_401_Unauthorized,
+                     );
+        return '';
+    }
+
+    my $workspace = $self->workspace;
+    my $update_request_hash = decode_json( $rest->getContent() );
+
+    if (my $uri = $update_request_hash->{customjs_uri}) {
+        $workspace->update(customjs_uri => $uri);
+    }
+
+    $rest->header( -status => HTTP_204_No_Content );
+    return '';
+}
 
 sub validate_resource_id {
     my( $self, $rest, $errors ) = @_;
