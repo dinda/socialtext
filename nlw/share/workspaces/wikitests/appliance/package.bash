eb31e588f7eb4da05557430a5cbeef867aab03e5
Ken Pier <ken.pier@socialtext.com>
2008-05-21 00:01:59 +0000

    Backporting appliance changes from trunk
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/iteration-2008-05-16@7560 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/workspaces/wikitests/appliance/package.bash
+++ nlw/share/workspaces/wikitests/appliance/package.bash
@@ -1,7 +1,10 @@
 #!/bin/sh
 # @COPYRIGHT@
 
-# Create a tarball from the latest wikitest files
+# Create  tarballs from the latest wikitest files
+
+
+cd  $ST_CURRENT/nlw/share/workspaces/wikitests/appliance
 
 if [ -e $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz ]; then
     echo DELETING old wikitests.1.tar.gz
@@ -12,9 +15,25 @@ echo CREATING new wikitests.1.tar.gz
 $ST_CURRENT/nlw/bin/st-admin export-workspace --w wikitests --dir $ST_CURRENT/nlw/share/workspaces/wikitests/
 cp $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance
 
-echo PACKAGING wikitests using dir2deb
-dir2deb --dir $ST_CURRENT/nlw/share/workspaces/wikitests/appliance  --p wikitests  --description Package wikitests for appliances -X package.bash -X appliance-wikitests.tar.gz -X ".svn" --strip-outer-dir
+if [ -e $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz ]; then
+    echo DELETING old calctests.1.tar.gz
+    rm $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz
+fi
 
-echo CREATING new appliance-wikitests.tar.gz
-tar zcf appliance-wikitests.tar.gz --exclude=".svn"  --exclude="*.deb" --exclude=appliance-wikitests.tar.gz --exclude="package.bash" *
+echo CREATING new calctests.1.tar.gz
+$ST_CURRENT/nlw/bin/st-admin export-workspace --w calctests --dir $ST_CURRENT/nlw/share/workspaces/calctests/
+cp $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance
+
+rm -rf $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr
+
+echo COPYING FILES FOR PACKAGING
+mkdir -p $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin
+cp  create-test-data-workspace fetch-test-data-tarball prep-wikitests run-wiki-tests $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/bin/
+
+mkdir -p $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/share/nlw/wikitests
+cp  do-wiki-tests  one-wiki-test  README  set-time  setup-selenium  wikitestfiles.zip  wikitests.1.tar.gz calctests.1.tar.gz $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr/share/nlw/wikitests/
+
+echo PACKAGING wikitests using dir2deb
+rm -f $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/wikitests*.deb
+dir2deb --dir $ST_CURRENT/nlw/share/workspaces/wikitests/appliance/usr  --package  wikitests  --description Package wikitests for appliances -X package.bash -X appliance-wikitests.tar.gz -X ".svn" 
 
