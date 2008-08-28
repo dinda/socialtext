dinda/socialtext

commit 55ec80c5a7c1d060ccdbefee3bb9da85d8b6291f

Don't load extra plugin lib paths, now that they are in a normal place
git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/iteration-2008-08-22@9416 593ce9bc-2028-0410-bb98-c566b643a948

nlw/lib/Socialtext/Pluggable/Adapter.pm
@@ -15,11 +15,6 @@ use Module::Pluggable search_path => ['Socialtext::Pluggable::Plugin'],
                       search_dirs => \@libs;
 use Socialtext::Pluggable::WaflPhrase;
 
-BEGIN {
-    our $code_base = Socialtext::AppConfig->code_base;
-    push @INC, glob("$code_base/plugin/*/lib");
-}
-
 sub AUTOLOAD {
     my ($self,$rest_handler,$args) = @_;
     my $type = ref($self)

commit 37752cbce15df2e688af46e0684bb6f23b461ba4

* Reverse the damage to dev-env caused by r9416.
------------------------------------------------------------------------
r9416 | lukec | 2008-08-27 16:32:49 -0700 (Wed, 27 Aug 2008) | 1 line
Don't load extra plugin lib paths, now that they are in a normal place
------------------------------------------------------------------------



git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/iteration-2008-08-22@9424 593ce9bc-2028-0410-bb98-c566b643a948

nlw/lib/Socialtext/Pluggable/Adapter.pm
@@ -15,6 +15,12 @@ use Module::Pluggable search_path => ['Socialtext::Pluggable::Plugin'],
                       search_dirs => \@libs;
 use Socialtext::Pluggable::WaflPhrase;
 
+BEGIN {
+    # This is still needed for dev-env -- Do Not Delete!
+    our $code_base = Socialtext::AppConfig->code_base;
+    push @INC, glob("$code_base/plugin/*/lib");
+}
+
 sub AUTOLOAD {
     my ($self,$rest_handler,$args) = @_;
     my $type = ref($self)