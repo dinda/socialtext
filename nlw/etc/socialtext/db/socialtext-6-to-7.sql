6b8e6039c31246fa059510a7d56c60d1293731d5
Luke Closs <gravatar@5thplane.com>
2008-08-29 18:28:01 +0000

    add missing fk contstraints
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/iteration-2008-08-08@9458 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/etc/socialtext/db/socialtext-6-to-7.sql
+++ nlw/etc/socialtext/db/socialtext-6-to-7.sql
@@ -69,6 +69,21 @@ CREATE INDEX ix_event_tag
 	    ON event (tag_name, at)
             WHERE (event_class = 'page' OR event_class = 'person');
 
+ALTER TABLE ONLY event
+    ADD CONSTRAINT event_actor_id_fk
+            FOREIGN KEY (actor_id)
+            REFERENCES "UserId"(system_unique_id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY event
+    ADD CONSTRAINT event_page_fk
+            FOREIGN KEY (page_workspace_id, page_id)
+            REFERENCES page(workspace_id, page_id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY event
+    ADD CONSTRAINT event_person_id_fk
+            FOREIGN KEY (person_id)
+            REFERENCES "UserId"(system_unique_id) ON DELETE CASCADE;
+
 ALTER TABLE tag_people__person_tags 
     DROP CONSTRAINT tag_people_fk;
 
