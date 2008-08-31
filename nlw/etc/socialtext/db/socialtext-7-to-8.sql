d5032d3d8b0c541c3a906bcb0197d21199d59b56
Luke Closs <gravatar@5thplane.com>
2008-08-31 01:58:16 +0000

    Rename the pkey contsraint after the table was renamed.
    This has already happened manually on staging, which is the only box with 08-22.
    
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/iteration-2008-08-22@9480 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/etc/socialtext/db/socialtext-7-to-8.sql
+++ nlw/etc/socialtext/db/socialtext-7-to-8.sql
@@ -9,6 +9,11 @@ ALTER TABLE person
 ALTER TABLE person
     ADD COLUMN small_photo_image bytea;
 
+-- Table was renamed, so we should re-name the pkey index to be consistent
+ALTER TABLE ONLY person_tag DROP CONSTRAINT tag_pkey CASCADE;
+ALTER TABLE ONLY person_tag ADD CONSTRAINT person_tag_pkey PRIMARY KEY (id);
+
+
 UPDATE "System"
    SET value = 8
  WHERE field = 'socialtext-schema-version';
