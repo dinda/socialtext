c185f9210845b308db0827fc4ad72997c161eb5e
Audrey Tang <audrey.tang@socialtext.com>
2008-09-06 10:17:24 +0000

    * Resolve {bz: 1109} by adding a hand-drawn previewButton.png   based on Aracunia's design.  Their existing CSS can't easily be   used because it'd require everything be upgraded to flexButtons.
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/iteration-2008-09-05@9704 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/share/skin/wikiwyg/javascript/Wikiwyg/Socialtext.js
+++ nlw/share/skin/wikiwyg/javascript/Wikiwyg/Socialtext.js
@@ -471,6 +471,8 @@ proto.preview_link_action = function() {
         .click(this.button_disabled_func());
     this.enable_edit_more = function() {
         jQuery(preview)
+            .removeClass('previewButton')
+            .addClass('editButton')
             .unbind('click')
             .click( function () {
                 if (jQuery("#contentRight").is(":visible")) 
@@ -497,6 +499,8 @@ proto.preview_link_reset = function() {
 
     var self = this;
     jQuery(preview)
+        .removeClass('editButton')
+        .addClass('previewButton')
         .unbind('click')
         .click( function() {
             return self.preview_link_action();
