00d00828fc802c3ddc97d8764081d6e2c6011517
Graham TerMarsch <graham.termarsch@socialtext.com>
2008-10-22 07:09:43 +0000

    New `ST::User::Cache->Remove()` method, which removes a homunculus from the cache.
    Further, calling `ST::User::Cache->Store($key,$val,$homey)` hunts down any
    existing cache entries that might be in place for a previously cached
    homunculus matching `$key=>$val`, and removes them from the cache
    automatically.
    
    This now makes it safe to call `ST::User::Cache->Store()` any time a user
    record is updated, without having to flush the entire cache.  The old
    homunculus is removed from the cache and then the new one is stored into the
    cache.
    
    
    git-svn-id: https://repo.socialtext.net:8999/svn/socialtext/branches/iteration-2008-10-17@10721 593ce9bc-2028-0410-bb98-c566b643a948

--- nlw/lib/Socialtext/User/Cache.pm
+++ nlw/lib/Socialtext/User/Cache.pm
@@ -6,8 +6,9 @@ use Socialtext::Cache;
 
 our $Enabled = 0;
 our %stats = (
-    fetch => 0,
-    store => 0,
+    fetch  => 0,
+    store  => 0,
+    remove => 0,
 );
 
 my %ValidKeys = (
@@ -31,12 +32,40 @@ sub Store {
     return unless $Enabled;
     return unless $ValidKeys{$key};
 
+    # remove any old cache entries for the homunculus defined by the given
+    # key/val pair
+    my $old_homey = _resolve_homunculus($key, $val);
+    if ($old_homey) {
+        # localize the stats so that the "remove" action doesn't get counted
+        local %stats = %stats;
+        Socialtext::User::Cache->Remove($old_homey);
+    }
+
     # proactively cache the homunculus against all valid keys, so he can be
     # found quickly/easily again in the future
-    $stats{store}++;
+    if ($homunculus) {
+        $stats{store}++;
+        foreach my $key (keys %ValidKeys) {
+            my $cache = Socialtext::Cache->cache("homunculus:$key");
+            $cache->set($homunculus->$key, $homunculus);
+        }
+    }
+}
+
+sub Remove {
+    my $class = shift;
+    return unless $Enabled;
+
+    # accept either "key=>val" pair to lookup homunculus, or the homunculus
+    # directly.
+    my $homunculus = _resolve_homunculus(@_);
+    return unless $homunculus;
+
+    # remove the homunculus from all caches
+    $stats{remove}++;
     foreach my $key (keys %ValidKeys) {
         my $cache = Socialtext::Cache->cache("homunculus:$key");
-        $cache->set($homunculus->$key, $homunculus);
+        $cache->remove($homunculus->$key);
     }
 }
 
@@ -50,6 +79,19 @@ sub Clear {
 
 sub ClearStats {
     map { $stats{$_} = 0 } keys %stats;
+}
+
+sub _resolve_homunculus {
+    # if given a "key=>val" pair, use that to find the homunculus in the cache
+    if (@_ > 1) {
+        my ($key, $val) = @_;
+        return unless $ValidKeys{$key};
+
+        my $cache = Socialtext::Cache->cache("homunculus:$key");
+        return $cache->get($val);
+    }
+    # only given one arg; must *be* the homunculus
+    return $_[0];
 }
 
 1;
