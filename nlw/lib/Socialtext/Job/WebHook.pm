55aa77312b8a16c3b17ca584603ce2dca3a57c5d
Luke Closs <gravatar@5thplane.com>
2009-01-07 23:20:15 -0800

    do not make the context double json encoded

--- nlw/lib/Socialtext/Job/WebHook.pm
+++ nlw/lib/Socialtext/Job/WebHook.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use base qw( TheSchwartz::Worker );
 use LWP::UserAgent;
-use Socialtext::JSON qw/encode_json/;
+use Socialtext::JSON qw/encode_json decode_json/;
 use Socialtext::Log qw/st_log/;
 
 sub work {
@@ -14,6 +14,7 @@ sub work {
     $ua->agent('Socialtext/WebHook');
 
     my $args = $job->arg;
+    $args->{event}{context} = decode_json($args->{event}{context});
     my $response = $ua->post( $args->{hook}{url},
         { payload => encode_json($args->{event}) },
     );
